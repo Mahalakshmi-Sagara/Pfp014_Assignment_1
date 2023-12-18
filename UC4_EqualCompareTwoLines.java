4aa80d340cddca450f8c566fe2c4386f49ca7073
Maha <maharangnath17@gmail.com>
2023-12-18 16:14:51 +0530

    Added EqualCompareTwoLines

---
+++ UC4_EqualCompareTwoLines.java
@@ -0,0 +1,27 @@
+package Pfp014_Assignment_1;
+
+import java.util.Scanner;
+
+public class UC4_EqualCompareTwoLines {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter x value ");
+		Integer x = sc.nextInt();
+		System.out.println("Enter y value ");
+		Integer y = sc.nextInt();
+		if(x.equals(y))
+		{ 
+			System.out.println("Same");
+		}
+		else if (x.compareTo(y)<0)
+		{
+			System.out.println("x is less than y");
+		}
+		else{
+			System.out.println("x is greater then y");
+		}
+
+	}
+
+}
